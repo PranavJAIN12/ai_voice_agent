4d4696d47803942c89b8da5b6c2d19f2ddc3e8b8
PranavJAIN12 <94856533+PranavJAIN12@users.noreply.github.com>
2025-06-09 22:53:23 +0530

    fileinput

--- app/(main)/dashboard/_components/ResMockInterview.js
+++ app/(main)/dashboard/_components/ResMockInterview.js
@@ -1,32 +1,47 @@
-
-
-
-import React from 'react'
+import { useFilePicker } from "use-file-picker";
+import React from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 
 const ResMockInterview = () => {
+  const { openFilePicker, filesContent, loading } = useFilePicker({
+    accept: ['.txt', '.pdf', '.docx']
+  });
+
   return (
     <div>
-      
-<Card>
-  <CardHeader>
-   <CardTitle>
-  📄 Resume-Based Mock Interview
-  <span className="ml-2 px-2 py-0.5 text-xs bg-yellow-400 text-black rounded-full">New</span>
-</CardTitle>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            📄 Resume-Based Mock Interview
+            <span className="ml-2 px-2 py-0.5 text-xs bg-yellow-400 text-black rounded-full">
+              New
+            </span>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p>
+            Upload your resume and get personalized AI interview questions based
+            on it.
+          </p>
+          {/* <Link href="/mock-interview/resume"> */}
+            <button className="mt-2 bg-blue-500 text-white px-4 py-2 rounded cursor-pointer" onClick={() => openFilePicker()}>
+              Try Now
+            </button>
+            <div>
 
-  </CardHeader>
-  <CardContent>
-    <p>Upload your resume and get personalized AI interview questions based on it.</p>
-    <Link href="/mock-interview/resume">
-      <button className="mt-2 bg-blue-500 text-white px-4 py-2 rounded">Try Now</button>
-    </Link>
-  </CardContent>
-</Card>
-
+            {filesContent.map((file,index)=>(
+              <div key={index}>
+                <h3 className="mt-3">{file.name}</h3>
+                {/* <div key={index}>{file.content}</div> */}
+              </div>
+            ))}
+            </div>
+          {/* </Link> */}
+        </CardContent>
+      </Card>
     </div>
-  )
-}
+  );
+};
 
-export default ResMockInterview
+export default ResMockInterview;
